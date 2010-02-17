f85a605f0d99b682c29e920c0be51aaee90efcd2
Miorel-Lucian Palii <mlpalii@gmail.com>
2010-02-17 15:54:22 -0400

    Automatically added license information.
    git-svn-id: http://melisma.googlecode.com/svn@10 1f62719a-04b0-11df-b019-0f202158fb84

--- trunk/src/net/irc/bot/BrainModule.java
+++ trunk/src/net/irc/bot/BrainModule.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009-2010 Miorel-Lucian Palii
+ * Copyright (C) 2010 Miorel-Lucian Palii
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
@@ -11,7 +11,6 @@
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
  */
-
 package net.irc.bot;
 
 import net.irc.event.AbstractIrcEventListener;
--- trunk/src/net/irc/cmd/UnknownCommand.java
+++ trunk/src/net/irc/cmd/UnknownCommand.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009-2010 Miorel-Lucian Palii
+ * Copyright (C) 2010 Miorel-Lucian Palii
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
